3ee8f0699c69cf9dfe9eafd9cea0778b6cfafccb
amelafili <31477792+amelafili@users.noreply.github.com>
2018-03-15 17:19:39 -0400

    added comments

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 var button = document.getElementById('choice')
-//
+
+/* button is added to an event, so this function works
+whenever button is clicked */
 button.addEventListener('click', function () {
+  // collecting theme used
   var theme = window.localStorage.getItem('savedTheme')
+// if theme is day, then display night theme when pressed
   if (theme === 'day') {
     theme = 'night'
+// if theme is on night, then display day when pressed
   } else {
     theme = 'day'
   }
   document.body.setAttribute('class', theme)
+// saves the last theme user was on
   window.localStorage.setItem('savedTheme', theme)
 }
 )
 
+// logs every time you hit the page
 var pagehits = window.localStorage.getItem('count')
 
+// if no visits, then is 0
 if (pagehits === null) {
   pagehits = 0
 }
-
+// everytime its visited, it adds one
 pagehits++
 
+// saves the amount of times the user visits
 window.localStorage.setItem('count', pagehits)
 
+// prints the number onto the page
 document.getElementById('count').innerHTML = pagehits
